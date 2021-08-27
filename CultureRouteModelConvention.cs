de9dcae4edc7fafba2b4d39e3642f683e60ec07a
ParadiseFallen <yjokerch@gmail.com>
2021-08-27 15:43:23 +0300

    Добавьте файлы проекта.

---
+++ CultureRouteModelConvention.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class CultureRouteModelConvention : IPageRouteModelConvention
+    {
+        public string RouteDataStringKey { get; init; } = "culture";
+        public void Apply(PageRouteModel model)
+        {
+            foreach (var selector in model.Selectors)
+            {
+                selector.AttributeRouteModel.Template = AttributeRouteModel.CombineTemplates($"{{{RouteDataStringKey}?}}", selector.AttributeRouteModel.Template);
+                //var selectorToAdd = new SelectorModel()
+                //{
+                //    AttributeRouteModel = new AttributeRouteModel
+                //    {
+                //        Template = ,
+                //        Order = -10,
+                //    }
+                //};
+                //model.Selectors.Add(selectorToAdd);
+            }
+        }
+    }
+}
